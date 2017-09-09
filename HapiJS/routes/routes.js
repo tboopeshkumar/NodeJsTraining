1a395f5b8152e7c989d8c4d98ac9b2b1ba185fd3
tboopeshkumar <tboopeshkumar@gmail.com>
2017-09-09 16:22:01 +0530

    Hapi js POST and DELETE

--- HapiJS/routes/routes.js
+++ HapiJS/routes/routes.js
@@ -38,6 +38,44 @@ module.exports = function(){
                     }
                 }
             }
+        },
+
+        {
+            method :'POST',
+            path : '/store',
+            config : {
+                handler : (req,reply)=>{
+                    console.log(req.payload);
+                    store.push({ name : req.payload.name, price: req.payload.price});
+                    reply ({  message : 'Successfully added the data', responseCode : 0});
+                },
+                validate : {
+                    payload :{
+                        name : joi.string(),
+                        price : joi.number()
+                    }
+                }
+            }
+        },
+        {
+            method :'DELETE',
+            path : '/store/{id}',
+            config : {
+                handler : (req,reply)=>{
+                    
+                    if(store.length <= req.params.id){
+                        return reply({message : 'product does not exists', responseCode : 1}).code(404);
+                    }
+                    store.splice(req.params.id,1);
+                    reply ({  message : 'Successfully deleted the data', responseCode : 0});
+                },
+                validate : {
+                    params : {
+                        id: joi.number()
+                    }
+                }
+            }        
         }
+
     ];
 }();
